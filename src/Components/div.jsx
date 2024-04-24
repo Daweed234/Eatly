e6080d2a49b813aa1624f148524a08bcc4e6b425
David <ntwalidavid31@gmail.com>
2024-04-24 09:16:27 +0200

    new page

---
+++ src/Components/div.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Div = () => {
+  return (
+    <>
+      <div className="h-screen">
+        <div>
+            
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Div;
